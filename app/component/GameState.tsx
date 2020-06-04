cc27c748662236d9ea6b461893254aafbfc5f142
Varesh Tapadia <varesh.tapadia@verifone.com>
2020-06-05 00:21:38 +0200

    WIP to support Game Play with computer

--- app/component/GameState.tsx
+++ app/component/GameState.tsx
@@ -2,12 +2,76 @@ import React from 'react';
 
 export class GameState {
   values: string[][];
-  constructor() {
-    this.values = Array(3).fill(Array(3).fill(null));
+  myTurn: boolean;
+  winner: string;
+  myPiece: string;
+  mode: string;
+  constructor(mode = "network", myPiece = "X") {
+    // this.values = Array(3).fill(Array(3).fill(undefined));
+    this.values = [...Array(3)].map(x=>Array(3).fill(undefined));
+    this.myTurn = true;
+    this.myPiece = myPiece;
+    this.mode = mode;
+    this.winner = "";
   }
 
-  setValue(row:number, col: number, value:string) {
+  playComputer() {
+    //Find a random number between 0 and 8
+    let random = Math.floor(Math.random() * 9);
+    let row:number = Math.floor(random/3);
+    let col:number = random%3;
+    if (!this.values[row][col]) {
+      this.values[row][col] = (this.myPiece==="X") ? "O" : "X";
+      console.log("Setting %s for %d, %d", this.values[row][col], row, col);
+    } else {
+      //try again to find another box
+      console.log("Randomizer skipped %d, %d with value %s filled", row, col, this.values[row][col]);
+      // alert("Box already filled");
+      this.playComputer();
+    }
+  }
+
+  hasFinished():boolean {
+    for (let i=0; i < 3; i++) {
+      if (this.values[i][0] == this.values[i][1] && 
+        this.values[i][1] == this.values[i][2] &&
+        this.values[i][0] != "") {
+          this.winner = this.values[i][0];
+          break;
+        }
+        if (this.values[0][i] == this.values[1][i] && 
+          this.values[1][i] == this.values[2][i] &&
+          this.values[0][i] != "") {
+            this.winner = this.values[0][i];
+            break;
+        }
+    }
+    if (this.values[0][0] == this.values[1][1] && 
+      this.values[1][1] == this.values[2][2] &&
+      this.values[0][0] != "") {
+        this.winner = this.values[0][0];
+    }
+    if (this.values[2][0] == this.values[1][1] && 
+      this.values[1][1] == this.values[0][2] &&
+      this.values[2][0] != "") {
+        this.winner = this.values[2][0];
+    }
+    return this.winner!="";
+  }
+
+  toggleTurn() {
+    this.myTurn = !this.myTurn;
+  }
+
+  setValue(row:number, col: number, value:string = this.myPiece) {
+    console.log("Setting myValue for %d, %d", row, col);
     this.values[row][col] = value;
+    this.toggleTurn();
+    if (this.mode == "offline") {
+      console.log("Playing computer turn");
+      this.playComputer();
+      this.toggleTurn();
+    }
   }
 
   getValue(row:number, col: number):string {
--- app/screens/Game.tsx
+++ app/screens/Game.tsx
@@ -6,14 +6,31 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 import { GameState } from '../component/GameState';
 
 export default function Game({ route, navigation }: GameProps) {
-  const game = new GameState();
+  const gameState = new GameState(route.params?.mode);
+
   return (
     <View style={styles.container}>
       <View style={{flex: 1}}></View>
       <View style={{flex: 3, flexDirection: 'row'}}>
         <View style={{flex: 1}}></View>
         <View style={{flex: 3, alignItems: 'stretch'}}>
-          <Board game={game}></Board>
+          <View style={styles.board}>
+            <View style={styles.boardRow}>
+              <Square row={0} col={0} game={gameState}></Square>
+              <Square row={0} col={1} game={gameState}></Square>
+              <Square row={0} col={2} game={gameState}></Square>
+            </View>
+            <View style={styles.boardRow}>
+              <Square row={1} col={0} game={gameState}></Square>
+              <Square row={1} col={1} game={gameState}></Square>
+              <Square row={1} col={2} game={gameState}></Square>
+            </View>
+            <View style={styles.boardRow}>
+              <Square row={2} col={0} game={gameState}></Square>
+              <Square row={2} col={1} game={gameState}></Square>
+              <Square row={2} col={2} game={gameState}></Square>
+            </View>
+          </View>
         </View>
         <View style={{flex: 1}}></View>
       </View>
@@ -33,16 +50,20 @@ export function Square(props: SquareProp) {
   const [value, setValue] = useState(String);
 
   function selected() {
-    setValue("X");
-    props.game?.setValue(props.row, props.col, "X");
+    if (props.game) {
+      if (!value) { 
+        props.game?.setValue(props.row, props.col);
+        setValue(props.game?.myPiece);
+      }
+    }
   }
 
   return (
     <TouchableHighlight style={styles.square} 
      onPress={selected}>
       <Text style={{textAlign: 'center'}}>
-        {value==="X" ? <Icon name="close" size={50} ></Icon> : null}
-        {value==="O" ? <Icon name="circle-o" size={40}></Icon> : null}
+        {value=="X" ? <Icon name="close" size={50} ></Icon> : null}
+        {value=="O" ? <Icon name="circle-o" size={40}></Icon> : null}
       </Text>
     </TouchableHighlight>
   );
